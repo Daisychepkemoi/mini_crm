3432cee9e193248eb84a814faefe4bfd45e23e98
daisychepkemoi <dchepkemoi408@gmail.com>
2019-07-21 14:06:36 +0300

    finalized

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.app')
 @section('content')
-<h1>hello</h1>
+<div style="background-image: url({{asset('storageo/back1.jpg')}}); height: 900px; background-size: cover; opacity:;">
+
+	<h1 style="margin-top: 200px; float: left; margin-left: 30%; color: yellow; font-size: 40px;font-weight: bolder;">Welcome to Mini_CRM</h1><br><br><br><br>
+	<h3 style="padding-top: 220px;  margin-left: 40%; color: yellow; font-size: 30px;font-weight: bold;"> Please <a href="{{ route('login') }}">Login</a>  To continue</h3> 
+</div>
+  
 @endsection
